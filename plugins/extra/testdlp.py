c8ccb91b58d3265b8bbc362b471dcee270c0f51e
fxsihadds <137089580+fxsihadds@users.noreply.github.com>
2025-02-17 21:59:55 +0600

    Update testdlp.py

--- plugins/extra/testdlp.py
+++ plugins/extra/testdlp.py
@@ -101,7 +101,7 @@ async def download_file(urls, name, message, output_dir="."):
     status = await message.reply("<b>⎚ `Downloading...`</b>")
     if "bongobd" in urls:
         ydl_opts = {
-            "outtmpl": f"{name}",
+            "outtmpl": os.path.join(output_dir, f"{name}"),
             "postprocessors": [
                 {
                     "key": "FFmpegVideoConvertor",  # Convert video format
